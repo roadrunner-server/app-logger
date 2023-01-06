9c6bfbcdef90828eda6aadfd46b78d1f13150b19
Valery Piashchynski <piashchynski.valery@gmail.com>
2023-01-06 10:37:33 +0100

    feature: endure v2 support

--- plugin.go
+++ plugin.go
@@ -4,18 +4,18 @@ import (
 	"go.uber.org/zap"
 )
 
-const (
-	name = "app"
-)
+const name = "app"
+
+type Logger interface {
+	NamedLogger(name string) *zap.Logger
+}
 
 type Plugin struct {
 	log *zap.Logger
 }
 
-func (p *Plugin) Init(log *zap.Logger) error {
-	p.log = new(zap.Logger)
-	*p.log = *log
-
+func (p *Plugin) Init(log Logger) error {
+	p.log = log.NamedLogger(name)
 	return nil
 }
 
